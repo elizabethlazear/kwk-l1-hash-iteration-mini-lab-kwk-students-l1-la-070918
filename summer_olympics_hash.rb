1114b177d7be4151013558cf6b5cb614655939d4
elizabethlazear <elizabethlazear04@gmail.com>
2018-07-16 18:45:12 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -25,27 +25,19 @@
 # add_a_key_value_pair
 
  
+place={"sydney", "Athens", "Beijing", "London", "Atlanta"}
+      year={"2000", "2004" "2008" "2012" "1996"}
 
-
-def iterate_through_hash
-  def place
-    def year
-      place={
-        "sydney", "Athens", "Beijing", "London", "Atlanta"}
-      year={ 
-        "2000", "2004" "2008" "2012" "1996"}
-        
-    end
-  end
-end
-      puts "The #{year} summer olympics took place in #{place} "
- end
+def iterate_through_hash(place,year)
+      puts "The #{year} summer olympics took place in #{place} 
+      end
+      
  
-  # Implement this method so that it iterates over the hash created in add_a_key_value_pair
-  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
+#   Implement this method so that it iterates over the hash created in add_a_key_value_pair
+#   and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
 
 
 
 # def iterate_through_keys
-  # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
-  # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
+#   Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
+#   into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
